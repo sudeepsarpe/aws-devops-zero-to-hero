3d168186062a656be61fb56f0dffdd5baf25324b
Sudeep Sarpe <106960811+sudeepsarpe@users.noreply.github.com>
2024-08-25 11:15:44 +0530

    add files

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -2,7 +2,8 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull sudeepsarpe/simple-python-flask-app
+
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 sudeepsarpe/simple-python-flask-app
